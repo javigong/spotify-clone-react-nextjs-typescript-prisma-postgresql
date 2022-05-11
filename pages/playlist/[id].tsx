2b62432b8ba4a5336df71716eb84a5c81bdb8125
Javier Gongora <jgongora@gmail.com>
2022-05-11 14:34:47 -0700

    Add user token validation before server-side rendering their playlists

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,4 +1,5 @@
 import GradientLayout from "../../components/gradientLayout";
+import SongsTable from "../../components/songsTable";
 import { validateToken } from "../../lib/auth";
 import prisma from "../../lib/prisma";
 
@@ -31,17 +32,28 @@ const Playlist = ({ playlist }) => {
       description={`${playlist.songs.length} songs`}
       image={`https://picsum.photos/400?random=${playlist.id}`}
     >
-      playlist
+      <SongsTable songs={playlist.songs} />
     </GradientLayout>
   );
 };
 
 export const getServerSideProps = async ({ query, req }) => {
-  const { id } = validateToken(req.cookies.TRAX_ACCESS_TOKEN);
+  let user;
+  try {
+    user = validateToken(req.cookies.TRAX_ACCESS_TOKEN);
+  } catch (e) {
+    return {
+      redirect: {
+        permanent: "false",
+        destination: "/signin",
+      },
+    };
+  }
+
   const [playlist] = await prisma.playlist.findMany({
     where: {
       id: +query.id,
-      userId: id,
+      userId: user.id,
     },
     include: {
       songs: {
