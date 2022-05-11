b2e59d1226baee21bdfb19d5417e278570b51837
Javier Gongora <jgongora@gmail.com>
2022-05-10 17:06:10 -0700

    Create auth.ts

---
+++ lib/auth.ts
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "./prisma";
+
+export const validateRoute = (handler) => {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    const { TRAX_ACCESS_TOKEN: token } = req.cookies;
+
+    if (token) {
+      let user;
+
+      try {
+        const { id } = jwt.verify(token, "hello");
+        user = await prisma.user.findUnique({
+          where: { id },
+        });
+
+        if (!user) {
+          throw new Error("Not real user");
+        }
+      } catch (e) {
+        res.status(401);
+        res.json({ error: "Not Authorized" });
+        return;
+      }
+      return handler(req, res, user);
+    }
+
+    res.status(401);
+    res.json({ error: "Not Authorized" });
+  };
+};
